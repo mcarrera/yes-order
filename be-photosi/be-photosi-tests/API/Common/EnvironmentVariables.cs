c45ae984d80849c3ea159bdf5e3803a27643535d
marco <mccarrera00@gmail.com>
2023-09-01 11:56:37 +0200

    Unit tests

--- be-photosi/be-photosi-tests/API/Common/EnvironmentVariables.cs
+++ be-photosi/be-photosi-tests/API/Common/EnvironmentVariables.cs
@@ -1,15 +1,43 @@
+using AutoFixture;
 using Xunit.Sdk;
 
 namespace be_photosi_tests.API.Common
 {
     public class EnvironmentVariables
     {
+        
         [Fact]
-        public void GetEnvironmentVariables()
+        public void GetDatabaseConnection_ReturnsString()
         {
+            // Arrange
+            Environment.SetEnvironmentVariable("DB_HOST", "DB_HOST");
+            Environment.SetEnvironmentVariable("DB_NAME", "DB_NAME");
+            Environment.SetEnvironmentVariable("DB_USER", "DB_USER");
+            Environment.SetEnvironmentVariable("DB_PASSWORD", "DB_PASSWORD");
+            // Act
             var connectionString = be_photosi_api.Common.EnvironmentVariables.GetDatabaseConnection();
-            Assert.NotNull(connectionString);
-            throw new SkipException("Test method is inconclusive");
+            // Assert
+            Assert.NotEmpty(connectionString);
         }
+
+     
+
+        [Fact]
+        public void GetDatabaseConnection_MissingEnvironmentVariable_ThrowsException()
+        {
+            // Arrange
+            Environment.SetEnvironmentVariable("DB_HOST", null);
+            Environment.SetEnvironmentVariable("DB_NAME", "DB_NAME");
+            Environment.SetEnvironmentVariable("DB_USER", "DB_USER");
+            Environment.SetEnvironmentVariable("DB_PASSWORD", "DB_PASSWORD");
+
+            // Act and Assert
+            Assert.Throws<Exception>(() =>
+            {
+                be_photosi_api.Common.EnvironmentVariables.GetDatabaseConnection();
+            });
+        }
+
+
     }
 }
